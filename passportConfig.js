8bbd3560693bfc830e155385c5e356d85b6d3ca4
Iggi <iggirex@gmail.com>
2016-09-18 11:17:24 -0600

    login working

--- passportConfig.js
+++ passportConfig.js
@@ -4,18 +4,19 @@ var knex = require('./db/knex') //comment check this if things not working
 var bcrypt = require('bcrypt')
 
 function getUser(username){
+  console.log("looking for this user: ", username)
   return knex('user_tbl')
-  .where(username)
+  .where("username", username)
 }
 
-passport.use( new LocalStrategy(function(username, password, done){
-  console.log("trying for local")
-  getUser({ username }) //check this for failures
-  .then(function(data){
-    console.log(data)
-    done(null, true, data)
-  })
-}))
+passport.use(
+  new LocalStrategy(function(username, password, done){
+    console.log("GOT LOCALthis username:  ", username, " and passwored:  ", password)
+    return getUser(username)
+      .then(function(data){
+        done(null, data)
+      })
+  }))
 
 passport.serializeUser(function(user, done){
   done(null, user)
--- routes/index.js
+++ routes/index.js
@@ -14,16 +14,17 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/register', function(req, res, next){
+  console.log(getUser(req.body.newUsername))
     getUser()
     .insert({
        username: req.body.newUsername,
-       password: bcrypt.hashSync(req.body.newPassword, 10)
+       password: bcrypt.hashSync(req.body.newPassword.toLowerCase(), 10)
      })
      .then(function(data){
+       console.log("Putting in THIS HASH: ", bcrypt.hashSync(req.body.newPassword, 10), "for this user:   ", req.body.newUsername, "HERE IS THE SAME HASH AGAIN:  ", bcrypt.hashSync(req.body.newPassword, 10))
        res.redirect("/")
      })
 })
-
 // router.post('/login',
 //   (req, res, next) => {
 //     passport.authenticate('local', (err, user, info) => {
@@ -37,11 +38,32 @@ router.post('/register', function(req, res, next){
 //   })
 
 router.post('/login', function(req, res, next){
-  console.log(req.body)
+  console.log("this is req.body:  ",req.body)
   passport.authenticate('local', function(err, user, info){
-    console.log(err, user, info)
-   return res.redirect('/users')
+    // user[0] is object coming thru, user[1] is undefined
+    if(user[0] === undefined){
+      console.log("getting this from LOocalStrategy: ",user)
+      return res.send("username does not exist in database")
+    } else {
+      var hash = bcrypt.hashSync(req.body.password, 10)
+      console.log("getting this from LOocalStrategy: ",user, "this is the HASH from LOGGING IN: ", hash, "THIS IS USER[0].password: ", user[0].password, "this is the user trying to get it:   ",req.body.username, "this is the password they are using: ", req.body.password)
+      var pass = req.body.password
+      var compare = bcrypt.compareSync(pass, user[0].password)
+      console.log("BCRYPT.COMPARE:", compare)
+      if(compare){
+        res.redirect('/home')
+      } else {
+        res.send("your passwerd wes inkerrekt")
+      }
+      // return res.send('YOURE FUCKING RITE!!!')
+    }
+  //   console.log(err, user, info)
+  //  return res.redirect('/users')
  })(req, res, next)
+})
+
+router.get('/home', function(req, res, next){
+  res.render("home")
 })
 
 // app.post('/auth', passport.authenticate('local'), function(req, res){
